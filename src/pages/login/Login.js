0124aa63bc604b22d698cd85a7b0ace828084eff
Moyeenwaz <90578165+Moyeenwaz@users.noreply.github.com>
2022-09-07 22:15:31 +0100

    3 files modified

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import './login.scss';
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <form>
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 export default Login;
